338782d3d8ec62e24088e0a6ba2dcabdfe99fe4a
Haris Nasri <harisstuuf@gmail.com>
2023-05-15 12:57:42 +0800

    child move

--- lib/components/my_button.dart
+++ lib/components/my_button.dart
@@ -10,16 +10,16 @@ class MyButton extends StatelessWidget {
     return Container(
         height: 40,
         width: 300,
-        child: Center(
-          child: Text(
-            btnName,
-            style: TextStyle(
-                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),
-          ),
-        ),
         decoration: BoxDecoration(
           color: Colors.deepPurple,
           borderRadius: BorderRadius.circular(5),
+        ),
+        child: Center(
+          child: Text(
+            btnName,
+            style: const TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.white, fontSize: 16),
+          ),
         ));
   }
 }
